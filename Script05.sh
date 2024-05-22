44f2a788879cbc699ef906bd0db377a557d59cae
javh07 <joseantonio.varela@uan.edu.mx>
2024-05-22 03:01:52 +0100

    Add files via upload

---
+++ Script05.sh
@@ -0,0 +1,7 @@
+#Autor: Jose Antonio Varela Hernández
+#Fecha: 12-03-2024
+#Descripción: Lectura de valor desde teclado
+#!/bin/bash
+echo "Archivo a buscar"
+read ARCHIVO
+echo "La variable es $ARCHIVO"
---
+++ Script06.sh
@@ -0,0 +1,7 @@
+#Autor: Jose Antonio Varela Hernández
+#Fecha: 12-03-2024
+#Descripción: Lectura de valor desde teclado y buscar una ruta 
+#!/bin/bash
+echo "Archivo a buscar"
+read ARCHIVO
+ls $HOME/$ARCHIVO
